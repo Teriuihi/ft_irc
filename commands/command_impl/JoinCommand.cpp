c96c13580b730a038e3b3d62c7a56db46b6d9e5a
Teriuihi <teriuih@alttd.com>
2023-03-17 03:32:55 +0100

    Fixed JoinCommand still need to move constants and might need to fix host

--- commands/command_impl/JoinCommand.cpp
+++ commands/command_impl/JoinCommand.cpp
@@ -4,25 +4,41 @@ string JoinCommand::getName() const {
 	return "JOIN";
 }
 
-void JoinCommand::execute(Server &server, string &command, int fd) {
-	string notconst = "<nick>!something@localhost JOIN <channel>\n"; //TODO move to constants
-	Channel *channel;
-	try {
-		channel = server.getChannel(command);
-	} catch (std::exception &e) {
-		cout << e.what() << endl;
-		return;
-	}
+void JoinCommand::execute(Server &server, string &command, int fd) { //TODO handle joining multiple channels in one command
+	string notconst = "<nick>!<username>@<hostname> JOIN <channel>\n"; //TODO move to constants
+	string notconst2 = ":<nick>!<username>@<hostname> JOIN <channel>\n";
 	User *user = server.getUser(fd);
 	if (user == NULL || !user->isAuthed()) {
 		//TODO ERROR
 		return;
 	}
-	Placeholder nickPlaceholder = Placeholder("nick", user->getNick());
-	Placeholder channelPlaceholder = Placeholder("channel", channel->getName());
+	Channel *channel;
+	try {
+		channel = server.getChannel(command);
+	} catch (std::exception &e) {
+		Template plt = Template(ErrorMessages::ERR_BADCHANNELKEY);
+		Placeholder channelP = Placeholder("channel", command);
+		plt.addPlaceholders(channelP);
+		string reply = plt.getString();
+		send(fd, reply.c_str(), reply.length(), 0);
+		return;
+	}
+	channel->addUser(user->getFd(), user);
+	Placeholder nickP = Placeholder("nick", user->getNick());
+	Placeholder usernameP = Placeholder("username", user->getUsername());
+	Placeholder channelP = Placeholder("channel", channel->getName());
+	Placeholder hostnameP = Placeholder("hostname", "localhost"); //TODO localhost might need to change
 	Template plt = Template(notconst);
-	plt.addPlaceholders(nickPlaceholder);
-	plt.addPlaceholders(channelPlaceholder);
+	Template plt2 = Template(notconst2);
+	plt.addPlaceholders(nickP);
+	plt.addPlaceholders(usernameP);
+	plt.addPlaceholders(channelP);
+	plt.addPlaceholders(hostnameP);
+	plt2.addPlaceholders(nickP);
+	plt2.addPlaceholders(usernameP);
+	plt2.addPlaceholders(channelP);
+	plt2.addPlaceholders(hostnameP);
 	string reply = plt.getString();
 	send(fd, reply.c_str(), reply.length(), 0);
+	channel->sendMessage(user, plt2.getString());
 }
