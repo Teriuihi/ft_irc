eb25532d6c3c2de8654a571b8226e8d4d6343a5f
Teriuihi <teriuih@alttd.com>
2023-03-17 03:32:12 +0100

    Send the right data back for UnknownCommand

--- commands/command_impl/UnknownCommand.cpp
+++ commands/command_impl/UnknownCommand.cpp
@@ -6,5 +6,10 @@ string UnknownCommand::getName() const {
 
 void UnknownCommand::execute(Server &server, string &command, int fd) {
 	server.getPollFd(); //Doing this to disable unused parameter warning since this is a school project which means there are no exceptions to rules that need exceptions
-	send(fd, command.c_str(), command.length(), 0);
+	Template err = Template(ErrorMessages::ERR_UNKNOWN_COMMAND);
+	Placeholder commandP = Placeholder("command", command);
+	err.addPlaceholders(commandP);
+
+	string reply = err.getString();
+	send(fd, reply.c_str(), reply.length(), 0);
 }
