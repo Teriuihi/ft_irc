4524342302566ad1d91932d483be1d740edbcbeb
Teriuihi <teriuih@alttd.com>
2023-03-17 03:31:52 +0100

    Added Username and RealName variables

--- irc_data/User.cpp
+++ irc_data/User.cpp
@@ -15,3 +15,19 @@ bool User::isAuthed() const {
 void User::setNick(string &nick) {
 	User::nick = nick;
 }
+
+const string &User::getUsername() const {
+	return username;
+}
+
+void User::setUsername(const string &username) {
+	User::username = username;
+}
+
+const string &User::getRealName() const {
+	return realName;
+}
+
+void User::setRealName(const string &realName) {
+	User::realName = realName;
+}
--- irc_data/User.hpp
+++ irc_data/User.hpp
@@ -8,14 +8,20 @@ class User {
 private:
 	int fd;
 	string nick;
+	string username;
+	string realName;
 	bool authed;
 public:
 	void setNick(string &nick);
+	const string &getUsername() const;
+	void setUsername(const string &username);
+	const string &getRealName() const;
+	void setRealName(const string &realName);
+	const string &getNick() const;
 
 	User(int fd, bool authed) : fd(fd), authed(authed) {}
 	int getFd() const;
 	bool isAuthed() const;
-	const string &getNick() const;
 };
 
 
