faf41004145be728243b56f03d7a86d5bf8dc246
Teriuihi <teriuih@alttd.com>
2023-03-18 00:15:57 +0100

    Re-ordered messages to match their codes

--- constants/ReplyMessages.hpp
+++ constants/ReplyMessages.hpp
@@ -7,12 +7,12 @@ namespace ReplyMessages {
 	const std::string RPL_YOURHOST = ":<server_hostname> 002 <nick> :Your host is <servername>, running version <ver>\n";
 	const std::string RPL_CREATED = ":<server_hostname> 003 <nick> :This server was created 16/03/2023\n";
 	const std::string RPL_MYINFO = ":<server_hostname> 004 <nick> :<servername> <version> <available user modes> <available channel modes>\n";
-	const std::string JOIN = ":<nick>!<username>@<hostname> JOIN <channel>\n";
+	const std::string RPL_LIST =  ":<server_hostname> 322 <channel> <users> :<topic>\n";
+	const std::string RPL_LISTEND =  ":<server_hostname> 323 :End of LIST\n";
 	const std::string RPL_TOPIC = ":<server_hostname> 332 <channel> :<topic>\n";
 	const std::string RPL_NAMREPLY = ":<server_hostname> 353 <channel> :<user_list>\n";
 	const std::string RPL_ENDOFNAMES = ":<server_hostname> 366 <channel> :End of NAMES list\n";
+	const std::string JOIN = ":<nick>!<username>@<hostname> JOIN <channel>\n";
 	const std::string PRIVMSG =  ":<nick>!<user>@<host> PRIVMSG <channel> :<message>\n";
-	const std::string RPL_LIST =  ":<server_hostname> 322 <channel> <users> :<topic>\n";
-	const std::string RPL_LISTEND =  ":<server_hostname> 323 :End of LIST\n";
 }
 #endif
