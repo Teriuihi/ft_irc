285dddf8265dcfce20c52e85d9c47f12d0f3f116
Teriuihi <teriuih@alttd.com>
2023-03-17 03:31:56 +0100

    Improved UserCommand  - can now receive full names with spaces  - correctly sets real name and username  - correctly uses nick, user, and host placeholders (host might need to change)

--- commands/command_impl/UserCommand.cpp
+++ commands/command_impl/UserCommand.cpp
@@ -8,20 +8,28 @@ string UserCommand::getName() const {
 //const string RPL_YOURHOST = ":irc.example.com 001 <nick> Your host is <servername>, running version <ver>";
 //const string RPL_CREATED = ":irc.example.com 001 <nick> This server was created 16/03/2023";
 //const string RPL_MYINFO = ":irc.example.com 001 <nick> <servername> <version> <available user modes> <available channel modes>";
+
+//received message is username status_bit_mask star? :real name
 void UserCommand::execute(Server &server, string &command, int fd) {
 	User *user = server.getUser(fd);
 	if (user == NULL || !user->isAuthed())
 		return;
 	std::vector<std::string> commandParts = splitString(command, " ");
-	if (commandParts.size() != 4) {
+	if (commandParts.size() < 4) {
 		std::cout << "Unexpected command length" << std::endl;
 		return;
 	}
-	user->setNick(commandParts[0]);
+	user->setUsername(commandParts[0]);
+	size_t pos = command.find(commandParts[3]);
+	if (pos != std::string::npos) {
+		user->setRealName(command.substr(pos));
+	} else {
+		user->setRealName(commandParts[3]);
+	}
 
 	Template welcome = Template(ReplyMessages::RPL_WELCOME);
-	Placeholder nickP = Placeholder("nick", commandParts[0]);
-	Placeholder userP = Placeholder("user", commandParts[3]);
+	Placeholder nickP = Placeholder("nick", user->getNick());
+	Placeholder userP = Placeholder("user", user->getUsername());
 	Placeholder hostP = Placeholder("host", "localhost"); //TODO fix
 	welcome.addPlaceholders(nickP);
 	welcome.addPlaceholders(userP);
